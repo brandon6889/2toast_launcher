f8fb6eaf08a1ddf405f0ea4b84e7712be0919435
Brandon McDonnell <bam@npmga.tk>
2016-12-24 16:53:14 -0500

    Add util/authTestLegacy.sh.

---
+++ util/authTestLegacy.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+user=$1
+pass=$2
+
+echo "Login:"
+echo -n "    "
+oldIFS="$IFS"
+IFS=':'
+set -- `curl -k -s -d "user=$user&pass=$pass&version=9999" https://auth.2toast.net/minecraft/login.php`
+IFS="$oldIFS"
+if [ $# -ne 4 ]
+then
+        echo "ERR: $@"
+        exit 1
+fi
+sid=$4
+user=$3
+echo "$1:$2:$3:$4"
+echo
+hash=$RANDOM
+echo "Join:"
+echo -n "    "
+curl -L "http://2toast.net/minecraft/joinserver.php?user=$user&sessionId=$sid&serverId=$hash"
+echo
+echo
+echo "Check:"
+echo -n "    "
+curl -L "http://2toast.net/minecraft/checkserver.php?user=$user&serverId=$hash"
+echo
