0e37115b3f0fe49260595e1c207e271b7c668a51
Brandon McDonnell <bam@npmga.tk>
2017-01-03 00:38:21 -0500

    JSON generator script supports subdirs.

--- util/genJson.sh
+++ util/genJson.sh
@@ -1,3 +1,3 @@
 # Leaves a trailing comma on last array element, just delete that when done.
 
-echo {; echo "  \"mods\": ["; for f in `find 1.4.7 -type f`; do echo "    {"; echo -n "      \"name\": \""; echo $f | awk -F/ '{ printf $2; }'; echo \",; echo "      \"size\": "$(ls -l $f | awk -F' ' '{printf $5; print ","}') ; echo "      \"hash\": \""$(sha1sum $f | awk -F' ' '{print $1}')\" ; echo "    },"; done; echo "  ]"; echo }
+(echo {; echo "  \"mods\": ["; for f in `find 1.10.2 -type f`; do echo "    {"; echo -n "      \"name\": \""; echo $f | awk -F/ '{ OFS="/"; $1=""; printf substr($0,2); }'; echo \",; echo "      \"size\": "$(ls -l $f | awk -F' ' '{printf $5; print ","}') ; echo "      \"hash\": \""$(sha1sum $f | awk -F' ' '{print $1}')\" ; echo "    },"; done; echo "  ]"; echo })>1.10.2.json
